bdd53f5542996c626314115f294a8a81919155d8
mikle <mikle>
2023-05-29 18:48:53 +0300

    4. Принципы раннего возврата и охранных выражений [#504875 #419308]

--- src/main/java/ru/job4j/early/PasswordValidator.java
+++ src/main/java/ru/job4j/early/PasswordValidator.java
@@ -7,30 +7,24 @@ public class PasswordValidator {
         if (password == null) {
             throw new IllegalArgumentException("Password can't be null");
         }
-
         boolean hasUpCase = false;
         boolean hasLowCase = false;
         boolean hasDigit = false;
         boolean hasSpecial = false;
         String upperPassword = password.toUpperCase();
-
         for (char symbol : password.toCharArray()) {
             if (Character.isAlphabetic(symbol) && Character.isUpperCase(symbol)) {
                 hasUpCase = true;
             }
-
             if (Character.isAlphabetic(symbol) && !Character.isUpperCase(symbol)) {
                 hasLowCase = true;
             }
-
             if (Character.isDigit(symbol)) {
                 hasDigit = true;
             }
-
             if (!Character.isAlphabetic(symbol) && !Character.isDigit(symbol)) {
                 hasSpecial = true;
             }
-
             if (hasUpCase && hasLowCase && hasDigit && hasSpecial) {
                 break;
             }
@@ -55,18 +49,14 @@ public class PasswordValidator {
                     "Password should contain at least one special symbol"
             );
         }
-
         for (String str : FORBIDDEN) {
-            str = str.toUpperCase();
-            if (upperPassword.indexOf(str) != -1) {
+            if (upperPassword.contains(str.toUpperCase())) {
                 throw new IllegalArgumentException("Password shouldn't contain substrings: qwerty, 12345, password, admin, user");
             }
         }
-
         if (password.length() < 8 || password.length() > 32) {
             throw new IllegalArgumentException("Password should be length [8, 32]");
         }
-
         return password;
     }
 }
