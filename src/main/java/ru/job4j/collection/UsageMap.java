bdf6d1e0a9fb88450df6dadac6cdab86fea34d6a
mikle <mikle>
2023-06-20 18:21:42 +0300

    0. Map, HashMap [#212652]

---
+++ src/main/java/ru/job4j/collection/UsageMap.java
@@ -0,0 +1,15 @@
+package ru.job4j.collection;
+
+import java.util.HashMap;
+
+public class UsageMap {
+    public static void main(String[] args) {
+        HashMap<String, String> hashMap = new HashMap<>();
+        hashMap.put("test@test.test", "Ivan Ivanov");
+        hashMap.put("test2@test.test", "Petr Petrov");
+        hashMap.put("test3@test.test", "Mikhail Mikhailov");
+        for (String key : hashMap.keySet()) {
+            System.out.println(hashMap.get(key));
+        }
+    }
+}
