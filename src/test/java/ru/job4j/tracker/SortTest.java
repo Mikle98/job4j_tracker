517707992764471195dee482796aef25d0602ca2
mikle <mikle>
2023-06-28 19:47:35 +0300

    0. Сортировка [#10096]

---
+++ src/test/java/ru/job4j/tracker/SortTest.java
@@ -0,0 +1,43 @@
+package ru.job4j.tracker;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class SortTest {
+    @Test
+    public void whenSortASCisTrue() {
+        List<Item> items = Arrays.asList(
+                new Item("Rabbit"),
+                new Item("Dog"),
+                new Item("Cat")
+        );
+        List<Item> expected = Arrays.asList(
+                new Item("Cat"),
+                new Item("Dog"),
+                new Item("Rabbit")
+        );
+        items.sort(new ItemAscByName());
+        assertThat(expected).isEqualTo(items);
+    }
+
+    @Test
+    public void whenSortDSCisTrue() {
+        List<Item> items = Arrays.asList(
+                new Item("Cat"),
+                new Item("Dog"),
+                new Item("Rabbit")
+        );
+        List<Item> expected = Arrays.asList(
+                new Item("Rabbit"),
+                new Item("Dog"),
+                new Item("Cat")
+        );
+        items.sort(new ItemDescByName());
+        assertThat(expected).isEqualTo(items);
+    }
+}
